8fcb4fdbc1d23c97fea6e65d1fae39b623c17ffe
Kalle Pusa <kalle.pusa@gmail.com>
2019-11-21 19:47:33 +0200

    Osa 2 tehty

---
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -0,0 +1,53 @@
+
+package ohtu.matkakortti;
+
+import ohtu.matkakortti.Maksukortti;
+import ohtu.matkakortti.Kassapaate;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class KassapaateTest {
+    
+    Kassapaate kassa;
+    Maksukortti kortti;
+    
+    @Before
+    public void setUp() {
+        kassa = new Kassapaate();
+        kortti = mock(Maksukortti.class);
+    }
+    
+    @Test
+    public void kortiltaVelotetaanHintaJosRahaaOn() {
+        when(kortti.getSaldo()).thenReturn(10);
+        kassa.ostaLounas(kortti);
+        
+        verify(kortti, times(1)).getSaldo();
+        verify(kortti).osta(eq(Kassapaate.HINTA));
+    }
+
+    @Test
+    public void kortiltaEiVelotetaJosRahaEiRiita() {
+        when(kortti.getSaldo()).thenReturn(4);
+        kassa.ostaLounas(kortti);
+        
+        verify(kortti, times(1)).getSaldo();
+        verify(kortti, times(0)).osta(anyInt());
+    }
+
+    @Test
+    public void lataaPositiivinenLataaRahaa() {
+        kassa.lataa(kortti, 5);
+        verify(kortti).lataa(5);
+    }
+
+    @Test
+    public void lataaNegatiivinenEiLataaRahaa() {
+        kassa.lataa(kortti, -5);
+        verify(kortti, never()).lataa(anyInt());
+    }
+      
+}
