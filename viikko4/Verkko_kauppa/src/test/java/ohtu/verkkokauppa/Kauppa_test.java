c7d50c35387cf8671d0b3d7ea057f2037eab7802
Kalle Pusa <kalle.pusa@gmail.com>
2019-11-23 12:36:10 +0200

    Part 3 tehty

--- viikko4/Verkko_kauppa/src/test/java/ohtu/verkkokauppa/Kauppa_test.java
+++ viikko4/Verkko_kauppa/src/test/java/ohtu/verkkokauppa/Kauppa_test.java
@@ -1,20 +1,31 @@
 package ohtu.verkkokauppa;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class Kauppa_test {
 
+   Pankki pankki;
+   Viitegeneraattori viite;
+   Varasto varasto = mock(Varasto.class);
+
+   @Before
+   public void setUp() {
+       pankki = mock(Pankki.class);
+       viite = mock(Viitegeneraattori.class);
+       varasto = mock(Varasto.class);
+
+   }
+
+
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
         // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
 
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
         // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
@@ -34,28 +45,73 @@ public class Kauppa_test {
 
     @Test
     public void aloitaAsiointi() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42,"12345", "33333-44455",5);
+    }
+
+    @Test
+    public void asioiKahdellaEriOstoksella() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "vesi", 6));
         // sitten testattava kauppa
         Kauppa k = new Kauppa(varasto, pankki, viite);
 
         // tehdään ostokset
         k.aloitaAsiointi();
-        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
         k.tilimaksu("pekka", "12345");
 
-        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto("pekka", 42,"12345", "33333-44455",11);
+    }
+
+    @Test
+    public void asioiKahdellaSamallaOstoksella() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42,"12345", "33333-44455",10);
+    }
+
+    @Test
+    public void asioiKahdellaEriOstoksellaToistEiOle() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(0);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "vesi", 6));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+
         verify(pankki).tilisiirto("pekka", 42,"12345", "33333-44455",5);
-        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
 }
