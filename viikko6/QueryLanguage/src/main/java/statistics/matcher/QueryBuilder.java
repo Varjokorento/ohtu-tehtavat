61944f1ac9dedf776a53f6c1eb52b6006928f457
Kalle Pusa <kalle.pusa@gmail.com>
2019-12-03 20:05:32 +0200

    Part 5 refaktoroiut

--- viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -8,7 +8,7 @@ public class QueryBuilder {
     private List<Matcher> matchers = new ArrayList<>();
 
     public Matcher build() {
-        Matcher allMatchers = new And(matchers.toArray(new Matcher[matchers.size()]));
+        Matcher allMatchers = new And(matchers.toArray(new Matcher[0]));
         this.matchers = new ArrayList<>();
         return allMatchers;
     }
